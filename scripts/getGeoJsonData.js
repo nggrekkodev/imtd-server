bd9dc0562ee1e37951d91e3eca4f71cc60c3d06f
nggrekkodev <nggrekkodev@gmail.com>
2020-02-24 17:29:44 +0100

    update

--- scripts/getGeoJsonData.js
+++ scripts/getGeoJsonData.js
@@ -1,47 +1,99 @@
 const fs = require('fs');
 const axios = require('axios');
+const excelToJson = require('convert-excel-to-json');
+const jsonToExcel = require('json2xls');
 
+// convert excel file to object
+const rawData = excelToJson({
+  sourceFile: `${__dirname}/data_v2.xlsx`
+});
+
+const data = {};
+
+// Loop through each type of interest point (entreprises, labos, formations)
+for (const property in rawData) {
+  // array of interest points of 1 type
+  const interestPoints = [];
+
+  // Loop through each interest point of a type
+  for (let i = 1; i < rawData[property].length; i++) {
+    const ip = {};
+
+    if (property === 'ENTREPRISES') {
+      ip['type'] = 'Entreprise';
+    } else if (property === 'LABOS') {
+      ip['type'] = 'Labo';
+    } else if (property === 'FORMATIONS') {
+      ip['type'] = 'Formation';
+    } else {
+    }
+
+    // Loop through each property of an interest point
+    for (const prop in rawData[property][i]) {
+      ip[rawData[property][0][prop]] = rawData[property][i][prop];
+    }
+
+    // console.log(ip);
+    // add interest point to array
+    interestPoints.push(ip);
+  }
+
+  // add array as a property : data["ENTREPRISES"] = [...]
+  data[property] = interestPoints;
+}
+
+// const xls = jsonToExcel(rawData['ENTREPRISES']);
+const xls = jsonToExcel({
+  name: 'Ivy Dickson',
+  date: '2013-05-27T11:04:15-07:00',
+  number: 10,
+  nested: {
+    field: 'foo'
+  }
+});
+fs.writeFileSync('results.xlsx', xls, 'binary');
+// console.log(data);
+
+/*
 // Read JSON FILE
 const rawData = fs.readFileSync(`${__dirname}/dataExcelToJson.json`, 'utf-8');
 // console.log(rawData);
 
 const data = JSON.parse(rawData);
 // console.log(data);
+*/
 
+/*
 const api = 'https://api-adresse.data.gouv.fr/search/';
-let querySearch = '';
-const queryLimit = 1;
-let queryPostCode = 0;
 
 const requests = [];
-const noGpsCoordinates = [];
+const ipWithCoordinates = [];
+const ipWithoutCoordinates = [];
 
-const getGeoJson = ip => {
+const getInterestPointCoordinates = ip => {
+  let querySearch = '';
+  if (ip.type === 'Entreprise') {
+    querySearch = `${ip.street} ${ip.location} ${ip.city}`;
+  } else if (ip.type === 'Labo') {
+    querySearch = `${ip.street} ${ip.fullName} ${ip.location}`;
+  } else if (ip.type === 'Formation') {
+    querySearch = `${ip.name} ${ip.street} ${ip.location}`;
+  }
+
   return axios
     .get(api, {
-      params: { q: `${ip.address1} ${ip.address2} ${ip.city} ${ip.postalCode}`, limit: queryLimit }
+      params: { q: `${querySearch}`, limit: 1, postcode: ip.postCode }
     })
     .then(res => {
       if (res.data.features.length > 0) {
         ip['coordinates'] = res.data.features[0].geometry.coordinates;
+        ip['latitude'] = res.data.features[0].geometry.coordinates[1];
+        ip['longitude'] = res.data.features[0].geometry.coordinates[0];
+        ipWithCoordinates.push(ip);
         console.log(`${ip.name} got coordinates : ${ip.coordinates}`);
       } else {
-        // noGpsCoordinates.push(ip);
-        // console.log(`${ip.name} got no coordinates`);
         console.log(`---> GPS coordinates not found for ${ip.name}, retry with name`);
-        return axios
-          .get(api, {
-            params: { q: `LaMcube ${ip.address1} ${ip.address2} ${ip.city} ${ip.postalCode}`, limit: queryLimit }
-          })
-          .then(res => {
-            if (res.data.features.length > 0) {
-              ip['coordinates'] = res.data.features[0].geometry.coordinates;
-              console.log(`${ip.name} got coordinates : ${ip.coordinates}`);
-            } else {
-              noGpsCoordinates.push(ip);
-              console.log(`${ip.name} got no coordinates`);
-            }
-          });
+        ipWithoutCoordinates.push(ip);
       }
     })
     .catch(err => console.log(err));
@@ -49,23 +101,30 @@ const getGeoJson = ip => {
 
 for (const property in data) {
   data[property].forEach(ip => {
-    requests.push(getGeoJson(ip));
+    if (!ip.hasOwnProperty('latitude') || !ip.hasOwnProperty('longitude')) {
+      requests.push(getInterestPointCoordinates(ip));
+    } else {
+      ip['coordinates'] = [ip['longitude'], ip['latitude']];
+      ipWithCoordinates.push(ip);
+    }
   });
 }
 
 axios.all(requests).then(
   axios.spread(function(acct, perms) {
     console.log('---> ALL PROMISES DONE');
-    const results = JSON.stringify(data);
+    const results = JSON.stringify(ipWithCoordinates);
     fs.writeFileSync('results.json', results);
     console.log('---> RESULTS WRITTEN');
     // POST TO DB RESULTS
 
-    if (noGpsCoordinates.length > 0) {
-      const errors = JSON.stringify(noGpsCoordinates);
+    if (ipWithoutCoordinates.length > 0) {
+      const errors = JSON.stringify(ipWithoutCoordinates);
       fs.writeFileSync('errors.json', errors);
       console.log('---> ERRORS WRITTEN');
       // NOT ENOUGH DATA TO GET A GPS COORDINATES
     }
   })
 );
+
+*/
