6e8e23e49466dffd171815e39f5432b0c0704ff8
nggrekkodev <nggrekkodev@gmail.com>
2020-04-15 20:54:59 +0200

    update geocoder

--- scripts/geocode.js
+++ scripts/geocode.js
@@ -2,20 +2,12 @@ const fs = require('fs');
 const axios = require('axios');
 const XLSX = require('xlsx');
 
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-
-// Read env variables from the file and save them into node env variables
-dotenv.config({ path: './config.env' });
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
 const sheetsJson = {}; // each property is a sheet
-const fileName = 'data_v2.xlsx';
-const fileOutput = 'data_v2_results.xlsx';
+const fileName = 'results.xlsx';
+const fileOutput = 'results.xlsx';
 let workbook;
 
-const frenchGovApi = 'https://api-adresse.data.gouv.fr/search/';
+const apiFR = 'https://api-adresse.data.gouv.fr/search/';
 const requests = [];
 const ipWithCoordinates = [];
 const ipWithoutCoordinates = [];
@@ -28,34 +20,33 @@ const longitudeMin = 1.378651;
 const getQuery = (ip, version) => {
   let query = `${ip.street} ${ip.city} ${ip.postCode}`;
 
-  if (version === 1) {
-    if (ip.type === 'Entreprise') {
-      query = `${ip.street} ${ip.city} ${ip.postCode}`;
-    } else if (ip.type === 'Laboratoire') {
-      query = `${ip.street} ${ip.city} ${ip.name}`;
-    } else if (ip.type === 'Formation') {
-      query = `${ip.street} ${ip.city} ${ip.name}`;
-    }
-  } else if (version === 2) {
-  }
+  // if (version === 1) {
+  //   if (ip.type === 'Entreprise') {
+  //     query = `${ip.street} ${ip.city} ${ip.postCode}`;
+  //   } else if (ip.type === 'Laboratoire') {
+  //     query = `${ip.street} ${ip.city} ${ip.name}`;
+  //   } else if (ip.type === 'Formation') {
+  //     query = `${ip.street} ${ip.city} ${ip.name}`;
+  //   }
+  // } else if (version === 2) {
+  // }
 
   return query;
 };
 
-// Get coordinates of an interest point (ip)
-const getCoordinatesFromFrenchGovApi = async (ip, version) => {
+// Get coordinates of a location
+const geocodeFR = async (ip, version) => {
   const querySearch = getQuery(ip, version);
 
   let res;
   try {
-    res = await axios.get(frenchGovApi, {
-      params: { q: `${querySearch}`, limit: 1 /*postcode: ip.postCode */ },
+    res = await axios.get(apiFR, {
+      params: { q: `${querySearch}`, limit: 1 },
     });
+    // console.log(res.data);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
-
-  // If the request gets a valid result
   if (
     res &&
     res.data.features.length > 0 &&
@@ -70,12 +61,8 @@ const getCoordinatesFromFrenchGovApi = async (ip, version) => {
 
     // Add the valid interest point to array of ip's with coordinates
     ipWithCoordinates.push(ip);
-    console.log(`${ip.name} got coordinates : [${ip.latitude}:${ip.longitude}]`);
-  }
-  // Request without a valid result
-  else {
-    console.log(`---> GPS coordinates not found for ${ip.name}, retry with name`);
-    // Add the invalid interest point to array of ip's without coordinates
+    // console.log(`${ip.name} got coordinates : [${ip.latitude}:${ip.longitude}]`);
+  } else {
     ipWithoutCoordinates.push(ip);
   }
   return true;
@@ -86,13 +73,13 @@ const writeFiles = () => {
     // Write valid results to data.json
     const results = JSON.stringify(ipWithCoordinates);
     fs.writeFileSync(`${__dirname}/data.json`, results);
-    console.log(`---> Valid results written in ${__dirname}/data.json`);
+    console.log(`-> data.json created`);
 
     // Write invalid results to dataIncomplete.json
     if (ipWithoutCoordinates.length > 0) {
       const errors = JSON.stringify(ipWithoutCoordinates);
       fs.writeFileSync(`${__dirname}/dataIncomplete.json`, errors);
-      console.log(`---> Invalid results written in ${__dirname}/dataIncomplete.json`);
+      console.log(`-> dataIncomplete.json created`);
     }
 
     // Convert each json sheet to a json array and add it has a property of sheetsJson
@@ -101,11 +88,24 @@ const writeFiles = () => {
     }
     // Write workbook data to a new xlsx file
     XLSX.writeFile(workbook, `${__dirname}/${fileOutput}`);
-    console.log(`---> New excel file created : ${__dirname}/${fileOutput}`);
+    console.log(`-> Excel file ${fileOutput} created`);
   } catch (error) {
     console.log(error);
   }
 };
+
+// try {
+//   fs.unlinkSync(`${__dirname}/${fileOutput}`);
+//   console.log(`-> ${fileOutput} deleted`);
+// } catch (error) {}
+try {
+  fs.unlinkSync(`${__dirname}/dataIncomplete.json`);
+  console.log(`-> dataIncomplete.json deleted`);
+} catch (error) {}
+try {
+  fs.unlinkSync(`${__dirname}/data.json`);
+  console.log(`-> data.json deleted`);
+} catch (error) {}
 
 // READ and CONVERT excel to json
 try {
@@ -125,52 +125,29 @@ try {
 for (const property in sheetsJson) {
   sheetsJson[property].forEach((ip) => {
     const typeLowerCase = property.toLowerCase();
+
+    // Add upper case to first character and last character 's'
     const type = typeLowerCase.charAt(0).toUpperCase() + typeLowerCase.slice(0, -1).slice(1);
-    console.log(type);
     ip['type'] = type;
 
     // If ip has no coordinates, push the promise to a promise array
     if (!ip.hasOwnProperty('latitude') || !ip.hasOwnProperty('longitude')) {
-      requests.push(getCoordinatesFromFrenchGovApi(ip, 1));
+      requests.push(geocodeFR(ip, 1));
     }
     // Else, form the coordinates with latitude and longitude from excel data
     else {
       // parse string to number with + operator
       ip['longitude'] = +ip['longitude'];
       ip['latitude'] = +ip['latitude'];
-      // ip['coordinates'] = [ip['longitude'], ip['latitude']];
       ipWithCoordinates.push(ip);
     }
+
+    const jsonIP = { ...ip };
+    const sectors = jsonIP.sector.split(',');
+    jsonIP.sector = sectors.map((el) => el.trim());
   });
 }
 
-// const connection = mongoose.connect(DB, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// });
-
-const connection = new Promise((resolve, reject) => {
-  mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    })
-    .then(() => {
-      console.log('Connection to DB');
-      resolve();
-    })
-    .catch((error) => {
-      console.log(err);
-      reject();
-    });
-});
-
-axios.all([...requests, connection]).then(() => {
-  console.log('All requests done');
+axios.all(requests).then(() => {
   writeFiles();
-  process.exit(1);
 });
