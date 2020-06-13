2fd88ce0bdcf1e9c3f636876f56a6586084aa767
nggrekkodev <nggrekkodev@gmail.com>
2020-06-13 07:57:16 +0200

    make it better

--- controllers/authentification.js
+++ controllers/authentification.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('./../models/User');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const AppError = require('./../utils/AppError');
 // const sendEmail = require('./../utils/email');
 
 // Return token signed with ID
